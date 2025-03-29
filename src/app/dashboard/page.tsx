823377de0ac5e7d273ef6629642c0c7b6e8cfcd7
Kushal Harsora <kushalharsora13@gmail.com>
2025-03-29 17:39:52 +0530

    Added Image to Admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=' w-[90vw] h-[90vh] flex justify-center items-center overflow-hidden text-4xl font-bold uppercase'>
+    <div
+      className='relative w-[90vw] max-md:w-screen h-[95vh] flex justify-center items-center overflow-hidden text-4xl max-md:text-xl text-white font-bold uppercase '
+      style={{ backgroundImage: 'url(/assets/admin-main.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
+      {/* Blur effect div */}
+      <div 
+        className='absolute inset-0 bg-black opacity-60' 
+        style={{ filter: 'blur(8px)' }}
+      ></div>
+
+      {/* Content */}
+      <div className='relative z-10'>
         Welcome to Avinnya Skin Clinic
+      </div>
     </div>
   )
 }
